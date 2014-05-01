9265067d0cf8bb6785906360cf0beb876167e6fb
takuki <takuki@users.sourceforge.net>
2014-05-01 20:33:34 +0000

    2014-05-01 tkamiya@us.fujitsu.com (Takuki Kamiya)
    - Added QNameValueEncodingTest.

---
+++ nagasenacs/test/Nagasena/Sax/QNameValueEncodingTest.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+using Org.System.Xml.Sax;
+
+using EXIDecoder = Nagasena.Proc.EXIDecoder;
+using AlignmentType = Nagasena.Proc.Common.AlignmentType;
+using EventDescription = Nagasena.Proc.Common.EventDescription;
+using EventDescription_Fields = Nagasena.Proc.Common.EventDescription_Fields;
+using EventType = Nagasena.Proc.Common.EventType;
+using GrammarOptions = Nagasena.Proc.Common.GrammarOptions;
+using GrammarCache = Nagasena.Proc.Grammars.GrammarCache;
+using Scanner = Nagasena.Proc.IO.Scanner;
+using EXISchema = Nagasena.Schema.EXISchema;
+using EXISchemaConst = Nagasena.Schema.EXISchemaConst;
+using TestBase = Nagasena.Schema.TestBase;
+using EXISchemaFactoryTestUtil = Nagasena.Scomp.EXISchemaFactoryTestUtil;
+
+namespace Nagasena.Sax {
+
+  [TestFixture]
+  [Category("Enable_Compression")]
+  public class QNameValueEncodingTest : TestBase {
+
+    private static readonly AlignmentType[] Alignments = new AlignmentType[] { 
+      AlignmentType.bitPacked, 
+      AlignmentType.byteAligned, 
+      //AlignmentType.preCompress, 
+      //AlignmentType.compress 
+    };
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Test cases
+    ///////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    /// A valid QName value matching ITEM_SCHEMA_CH where the associated
+    /// datatype is xsd:QName.
+    /// </summary>
+    [Test]
+    public virtual void testValidQName() {
+      EXISchema corpus = EXISchemaFactoryTestUtil.getEXISchema("/QName.xsc", this);
+
+      GrammarCache grammarCache = new GrammarCache(corpus, GrammarOptions.STRICT_OPTIONS);
+
+      string[] xmlStrings;
+      String[] values = {
+          "  foo:abc ", 
+          " abc ", 
+      };
+      String[] resultValues = {
+          "  foo:abc ", 
+          " abc ", 
+      };
+
+      int i;
+      xmlStrings = new string[values.Length];
+      for (i = 0; i < values.Length; i++) {
+        xmlStrings[i] = "<foo:QName xmlns:foo='urn:foo'>" + values[i] + "</foo:QName>\n";
+      };
+
+      foreach (AlignmentType alignment in Alignments) {
+        for (i = 0; i < xmlStrings.Length; i++) {
+          Transmogrifier encoder = new Transmogrifier();
+          EXIDecoder decoder = new EXIDecoder();
+          Scanner scanner;
+
+          encoder.AlignmentType = alignment;
+          decoder.AlignmentType = alignment;
+
+          encoder.GrammarCache = grammarCache;
+          MemoryStream baos = new MemoryStream();
+          encoder.OutputStream = baos;
+
+          byte[] bts;
+          int n_events;
+
+          encoder.encode(new InputSource<Stream>(string2Stream(xmlStrings[i])));
+
+          bts = baos.ToArray();
+
+          decoder.GrammarCache = grammarCache;
+          decoder.InputStream = new MemoryStream(bts);
+          scanner = decoder.processHeader();
+
+          EventDescription exiEvent;
+          n_events = 0;
+
+          EventType eventType;
+
+          exiEvent = scanner.nextEvent();
+          Assert.AreEqual(EventDescription_Fields.EVENT_SD, exiEvent.EventKind);
+          eventType = exiEvent.getEventType();
+          Assert.AreEqual(EventType.ITEM_SD, eventType.itemType);
+          ++n_events;
+
+          exiEvent = scanner.nextEvent();
+          Assert.AreEqual(EventDescription_Fields.EVENT_SE, exiEvent.EventKind);
+          eventType = exiEvent.getEventType();
+          Assert.AreEqual(EventType.ITEM_SE, eventType.itemType);
+          Assert.AreEqual("QName", eventType.name);
+          Assert.AreEqual("urn:foo", eventType.uri);
+          ++n_events;
+
+          exiEvent = scanner.nextEvent();
+          Assert.AreEqual(EventDescription_Fields.EVENT_CH, exiEvent.EventKind);
+          Assert.AreEqual(resultValues[i], exiEvent.Characters.makeString());
+          eventType = exiEvent.getEventType();
+          Assert.AreEqual(EventType.ITEM_SCHEMA_CH, eventType.itemType);
+          if (alignment == AlignmentType.bitPacked || alignment == AlignmentType.byteAligned) {
+            int tp = scanner.currentState.contentDatatype;
+            Assert.AreEqual(EXISchemaConst.QNAME_TYPE, corpus.getSerialOfType(tp));
+          }
+          ++n_events;
+
+          exiEvent = scanner.nextEvent();
+          Assert.AreEqual(EventDescription_Fields.EVENT_EE, exiEvent.EventKind);
+          eventType = exiEvent.getEventType();
+          Assert.AreEqual(EventType.ITEM_EE, eventType.itemType);
+          ++n_events;
+
+          exiEvent = scanner.nextEvent();
+          Assert.AreEqual(EventDescription_Fields.EVENT_ED, exiEvent.EventKind);
+          eventType = exiEvent.getEventType();
+          Assert.AreEqual(EventType.ITEM_ED, eventType.itemType);
+          ++n_events;
+
+          Assert.AreEqual(5, n_events);
+        }
+      }
+    }
+
+  }
+
+}
