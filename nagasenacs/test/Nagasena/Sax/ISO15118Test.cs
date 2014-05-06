766b009b28e287f05c6ec1aedfc4e7ea99a88cbd
takuki <takuki@users.sourceforge.net>
2014-05-06 18:20:53 +0000

    2014-05-06 tkamiya@us.fujitsu.com (Takuki Kamiya)
    - Added ISO15118Test.

---
+++ nagasenacs/test/Nagasena/Sax/ISO15118Test.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+using Org.System.Xml.Sax;
+
+using EXIDecoder = Nagasena.Proc.EXIDecoder;
+using AlignmentType = Nagasena.Proc.Common.AlignmentType;
+using EventDescription = Nagasena.Proc.Common.EventDescription;
+using EventDescription_Fields = Nagasena.Proc.Common.EventDescription_Fields;
+using EventType = Nagasena.Proc.Common.EventType;
+using EventTypeList = Nagasena.Proc.Common.EventTypeList;
+using GrammarOptions = Nagasena.Proc.Common.GrammarOptions;
+using GrammarCache = Nagasena.Proc.Grammars.GrammarCache;
+using Scanner = Nagasena.Proc.IO.Scanner;
+using EXISchema = Nagasena.Schema.EXISchema;
+using TestBase = Nagasena.Schema.TestBase;
+using EXISchemaFactoryTestUtil = Nagasena.Scomp.EXISchemaFactoryTestUtil;
+
+namespace Nagasena.Sax {
+
+  [TestFixture]
+  [Category("Enable_Compression")]
+  public class ISO15118Test : TestBase {
+
+    private static readonly AlignmentType[] Alignments = new AlignmentType[] { 
+      AlignmentType.bitPacked, 
+      AlignmentType.byteAligned, 
+      //AlignmentType.preCompress, 
+      //AlignmentType.compress 
+    };
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Test cases
+    ///////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    /// EXI test cases of ISO15118.
+    /// </summary>
+    [Test]
+    public virtual void testISO15118() {
+      EXISchema corpus = EXISchemaFactoryTestUtil.getEXISchema("/iso15118/V2G_CI_MsgDef.xsc", this);
+
+      GrammarCache grammarCache = new GrammarCache(corpus, GrammarOptions.STRICT_OPTIONS);
+
+      foreach (AlignmentType alignment in Alignments) {
+        Transmogrifier encoder = new Transmogrifier();
+        EXIDecoder decoder = new EXIDecoder();
+        Scanner scanner;
+        InputSource inputSource;
+
+        encoder.AlignmentType = alignment;
+        decoder.AlignmentType = alignment;
+
+        encoder.GrammarCache = grammarCache;
+        MemoryStream baos = new MemoryStream();
+        encoder.OutputStream = baos;
+
+        Uri url = resolveSystemIdAsURL("/iso15118/SessionSetupRes.xml");
+        FileStream inputStream = new FileStream(url.LocalPath, FileMode.Open);
+        inputSource = new InputSource<Stream>(inputStream, url.ToString());
+
+        byte[] bts;
+        int n_events;
+
+        encoder.encode(inputSource);
+        inputStream.Close();
+
+        bts = baos.ToArray();
+
+        decoder.GrammarCache = grammarCache;
+        decoder.InputStream = new MemoryStream(bts);
+        scanner = decoder.processHeader();
+
+        EventDescription exiEvent;
+        n_events = 0;
+
+        EventType eventType;
+        EventTypeList eventTypeList;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_SD, exiEvent.EventKind);
+        eventType = exiEvent.getEventType();
+        Assert.AreSame(exiEvent, eventType);
+        Assert.AreEqual(0, eventType.Index);
+        eventTypeList = eventType.EventTypeList;
+        Assert.AreEqual(1, eventTypeList.Length);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_SE, exiEvent.EventKind);
+        Assert.AreEqual("V2G_Message", exiEvent.Name);
+        Assert.AreEqual("urn:iso:15118:2:2013:MsgDef", exiEvent.URI);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_SE, exiEvent.EventKind);
+        Assert.AreEqual("Header", exiEvent.Name);
+        Assert.AreEqual("urn:iso:15118:2:2013:MsgDef", exiEvent.URI);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_SE, exiEvent.EventKind);
+        Assert.AreEqual("SessionID", exiEvent.Name);
+        Assert.AreEqual("urn:iso:15118:2:2013:MsgHeader", exiEvent.URI);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_CH, exiEvent.EventKind);
+        Assert.AreEqual("3031323334353637", exiEvent.Characters.makeString());
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_EE, exiEvent.EventKind);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_EE, exiEvent.EventKind);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_SE, exiEvent.EventKind);
+        Assert.AreEqual("Body", exiEvent.Name);
+        Assert.AreEqual("urn:iso:15118:2:2013:MsgDef", exiEvent.URI);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_SE, exiEvent.EventKind);
+        Assert.AreEqual("SessionSetupRes", exiEvent.Name);
+        Assert.AreEqual("urn:iso:15118:2:2013:MsgBody", exiEvent.URI);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_SE, exiEvent.EventKind);
+        Assert.AreEqual("ResponseCode", exiEvent.Name);
+        Assert.AreEqual("urn:iso:15118:2:2013:MsgBody", exiEvent.URI);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_CH, exiEvent.EventKind);
+        Assert.AreEqual("OK", exiEvent.Characters.makeString());
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_EE, exiEvent.EventKind);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_SE, exiEvent.EventKind);
+        Assert.AreEqual("EVSEID", exiEvent.Name);
+        Assert.AreEqual("urn:iso:15118:2:2013:MsgBody", exiEvent.URI);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_CH, exiEvent.EventKind);
+        Assert.AreEqual("FRA23E45B78C", exiEvent.Characters.makeString());
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_EE, exiEvent.EventKind);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_EE, exiEvent.EventKind);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_EE, exiEvent.EventKind);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_EE, exiEvent.EventKind);
+        ++n_events;
+
+        exiEvent = scanner.nextEvent();
+        Assert.AreEqual(EventDescription_Fields.EVENT_ED, exiEvent.EventKind);
+        ++n_events;
+
+        Assert.IsNull(scanner.nextEvent());
+
+        Assert.AreEqual(19, n_events);
+      }
+    }
+
+  }
+
+}
