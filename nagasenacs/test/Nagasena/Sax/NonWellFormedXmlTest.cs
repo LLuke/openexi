1043a6f278ea25ddd855b8457bbf17256bb08d0d
takuki <takuki@users.sourceforge.net>
2014-05-07 00:25:46 +0000

    2014-05-06 tkamiya@us.fujitsu.com (Takuki Kamiya)
    - Added NonWellFormedXmlTest.

---
+++ nagasenacs/test/Nagasena/Sax/NonWellFormedXmlTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+using Org.System.Xml.Sax;
+
+using GrammarOptions = Nagasena.Proc.Common.GrammarOptions;
+using GrammarCache = Nagasena.Proc.Grammars.GrammarCache;
+using TestBase = Nagasena.Schema.TestBase;
+
+namespace Nagasena.Sax {
+
+  [TestFixture]
+  public class NonWellFormedXmlTest : TestBase {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Test cases
+    ///////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    /// Not a well-formed XML 
+    /// </summary>
+    [Test]
+    public virtual void testNoEndTag() {
+
+      string xmlString;
+      xmlString = "<?xml version='1.0' ?>" +
+        "<None xmlns='urn:foo'>\n" +
+        "  <A>\n" +
+        "</None>";
+
+      Transmogrifier encoder = new Transmogrifier();
+
+      encoder.GrammarCache = new GrammarCache(GrammarOptions.DEFAULT_OPTIONS);
+      MemoryStream baos = new MemoryStream();
+      encoder.OutputStream = baos;
+
+      try {
+        encoder.encode(new InputSource<Stream>(string2Stream(xmlString)));
+      }
+      catch (TransmogrifierException te) {
+        ILocator locator = te.Locator;
+        Assert.AreEqual(3, locator.LineNumber);
+        return;
+      }
+      Assert.Fail();
+    }
+
+  }
+
+}
